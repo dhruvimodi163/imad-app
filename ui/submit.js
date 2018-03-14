289f2f7c95876dbf17a5b7f7f5cc93fde2327591
dhruvimodi163 <36543036+dhruvimodi163@users.noreply.github.com>
2018-03-14 17:53:03 +0530

    [imad-console] Updates ui/submit.js

--- ui/submit.js
+++ ui/submit.js
@@ -1,1 +1,9 @@
-mkjjnj
+var express = require('express');
+var http =require('http');
+var bodyParser= require('bodt_parser');
+var app = express();
+var urlencodedParser = bodyParser.urlencoced({ extended: true});
+ app.post('/ui/server.js', urlencodedParser, function(req,res){
+    res.send("You entered the following data");
+     
+ });
