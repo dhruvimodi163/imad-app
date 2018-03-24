a5627106d98d70d24d44376487e9ea8bc83e1211
dhruvimodi163 <36543036+dhruvimodi163@users.noreply.github.com>
2018-03-24 22:15:40 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,10 @@
-console.log('Loaded!');
+function count(){
+    var counter=0;
+    var request=new XMLHttpRequest();
+    request.onreadystatechange = function(){
+     if (request.readyState == XMLHttpRequest.DONE){
+         counter=counter+1;
+         res.send(counter.toString());
+     }  
+    };
+}
